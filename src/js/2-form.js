328215e236ef30b44823a933fa32e22f24008eda
Larimar4you <Zajanezlaja@gmail.com>
2025-02-01 21:52:47 +0100

    git push

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,30 @@
 'use strict';
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {
+  email: '',
+  message: '',
+};
+
+form.email.value = formData.email;
+form.message.value = formData.message;
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+  formData = { email: '', message: '' };
+});
